af24354accd35b9e6555e06f9da31d75e7d772a4
Fischaela <michaela.lehr@geildanke.com>
2018-04-13 18:53:40 +0200

    MOD chart component DEL default primitives

--- components/chart.js
+++ components/chart.js
@@ -10,5 +10,34 @@ AFRAME.registerComponent('chart', {
     plane.setAttribute('height', '1');
     plane.setAttribute('position', '0 0 0');
     this.el.appendChild(plane);
+
+    let data = d3.range(10).map(function (d, i) {
+      return { v: Math.random() };
+    });
+
+    let entities = d3.select(this.el)
+      .selectAll('a-entity')
+      .data(data);
+
+    let newobject = entities.enter()
+      .append('a-entity');
+    newobject.append('a-cylinder')
+      .attr('color', this.data.color)
+      .attr('radius', 0.025)
+      .attr('height', function (d, i) {
+        return d.v;
+      })
+      .attr('position', function (d, i) {
+        return  (i * 0.05 - 0.25 + 0.025) + ' ' + d.v / 2 + ' 0';
+      });
+  }
+});
+
+AFRAME.registerPrimitive('a-chart', {
+  defaultComponents: {
+    chart: {}
+  },
+  mappings: {
+    color: 'chart.color'
   }
 });
