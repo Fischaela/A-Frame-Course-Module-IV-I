e9d67f79df14b03ddad97fd1641fa7f59805f1be
Fischaela <michaela.lehr@geildanke.com>
2018-04-13 18:40:56 +0200

    ADD plane to chart component

--- components/chart.js
+++ components/chart.js
@@ -2,5 +2,13 @@ AFRAME.registerComponent('chart', {
   schema: {
     color: {type: 'string', default: 'tomato'}
   },
-  init: function () {}
+  init: function () {
+    let plane = document.createElement('a-plane');
+    plane.setAttribute('rotation', '-90 0 0')
+    plane.setAttribute('color', 'grey');
+    plane.setAttribute('width', '1');
+    plane.setAttribute('height', '1');
+    plane.setAttribute('position', '0 0 0');
+    this.el.appendChild(plane);
+  }
 });
